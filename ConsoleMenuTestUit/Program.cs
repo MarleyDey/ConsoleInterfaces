64a10cb38a6f4910d7fe3b6900f577f6a63481f9
marleydey <marley@deyy.co.uk>
2019-08-18 23:59:24 +0100

    Added initial build of libarary

--- ConsoleMenuTestUit/Program.cs
+++ ConsoleMenuTestUit/Program.cs
@@ -7,15 +7,18 @@ namespace ConsoleMenuTestUit
     {
         static void Main(string[] args)
         {
-
+          
             TickBoxMenu tickBoxMenu = new TickBoxMenu();
 
             tickBoxMenu.BoxOptionSelect += OnBoxOptionSelect;
 
-            tickBoxMenu.SetBoxOptions("Bob", "Tony", "Marley");
+            tickBoxMenu.SetBoxOptions("This is the first tick box", "The second tick box", "And the third");
+            tickBoxMenu.DisplayMenu("This is a tick box title:");
+        }
 
-            tickBoxMenu.DisplayMenu("Select A big boi:");
-
+        static void OnOptionSelect(object sender, OptionsMenu.OptionSelectedEventArgs e)
+        {
+            Console.WriteLine("You have selected option " + e.option);
         }
 
         static void OnBoxOptionSelect(object sender, TickBoxMenu.BoxOptionSelectedEventArgs e)
@@ -26,5 +29,15 @@ namespace ConsoleMenuTestUit
                 Console.WriteLine("- " + option);
             }
         }
+
+        /*
+         TickBoxMenu tickBoxMenu = new TickBoxMenu();
+
+            tickBoxMenu.BoxOptionSelect += OnBoxOptionSelect;
+
+            tickBoxMenu.SetBoxOptions("Bob", "Tony", "Marley");
+
+            tickBoxMenu.DisplayMenu("Select A big boi:");
+    */
     }
 }
